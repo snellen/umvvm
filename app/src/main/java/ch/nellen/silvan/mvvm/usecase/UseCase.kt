06aa8ce40a8a2b7080c83f1623d9f463903aba0a
Silvan Nellen <silvan.nellen@nab.com.au>
2019-10-01 13:03:15 +1000

    [MVVM] Interface definition of use cases.

---
+++ app/src/main/java/ch/nellen/silvan/mvvm/usecase/UseCase.kt
@@ -0,0 +1,18 @@
+package ch.nellen.silvan.mvvm.usecase
+
+/**
+ * Interface definition for use cases. A use case is a single purpose, reusable class that contains
+ * no state variables. Think of a use case as a building block used to construct the logic of a BaseViewModel.
+ *
+ * @param InputType the type of the parameter for this use case. Use Unit for use cases that do not take a parameter
+ * @param OutputType the type of the result returned when executing this use case.
+ */
+interface UseCase<InputType, OutputType> {
+
+    /**
+     * Executes this use case, using input to produce an output of type OutputType. Execution may
+     * suspend if this involves long running operations, such as an API call or processing large
+     * amounts of data.
+     */
+    suspend fun execute(input: InputType): OutputType
+}
